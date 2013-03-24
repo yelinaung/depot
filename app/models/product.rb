dee0a0b98a2be3efb315e6bc27762ee5d33c7128
mgpyone <mgpyone@mysteryzillion.com>
2013-03-24 16:34:39 +0630

    Added validation in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
+    attr_accessible :description, :image_url, :price, :title
+    validates :title, :description, :image_url, :presence => true
+    validates :price, :numericality => {:greater_than_equal_to => 0.01 }
+    validates :title, :uniqueness => true
 end
